437c5f71ca416b713389dd17ae41955924cafd6a
Pozitiv4i <gribochok52@gmail.com>
2021-05-31 20:03:17 +0300

    completed task-2, started task-3

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let box1 = document.getElementById('box-1');
 let randomColor = ["red", "pink", "yellow", "orange", "green"];
+let box2 = document.getElementById('task-2');
+let paragraph = document.getElementById('task-3')
 box1.addEventListener("click", function() {
     box1.style.backgroundColor = randomColor[Math.floor(Math.random(randomColor) * randomColor.length)]
+});
+box2.addEventListener('click', event => { 
+    event.target.style.borderColor = "red"
+});
+paragraph.addEventListener('click', event => { 
+    event.target.style.color = "red"
 });
