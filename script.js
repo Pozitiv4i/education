74e210f20e55cd812f9a27fcf334a6641d2d0155
Pozitiv4i <gribochok52@gmail.com>
2021-06-03 19:57:52 +0300

    started task karousel

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
-const ContentPopUp = document.getElementById('img1');
-const LockPopUp = document.getElementById('lock-popup');
-const popup = document.getElementById('popup1');
-ContentPopUp.addEventListener("click", function() {
+const imgContainer = document.getElementById('img-container')
+const popupContent = document.querySelector(".b-popup-content")
+const closeButtonPopup = document.getElementById('lock-popup');
+const popup = document.getElementById('popup');
+const carousel = document.querySelector("[data-target='carousel']");
+const card = carousel.querySelector("[data-target='card']");
+const leftButton = document.querySelector("[data-action='slideLeft']");
+const rightButton = document.querySelector("[data-action='slideRight']");
+
+const carouselWidth = carousel.offsetWidth;
+const cardStyle = card.currentStyle || window.getComputedStyle(card)
+const cardMarginRight = Number(cardStyle.marginRight.match(/\d+/g)[0]);
+
+
+const cardCount = carousel.querySelectorAll("[data-target='card']").length;
+
+
+let offset = 0;
+const maxX = -((cardCount / 2) * carouselWidth + 
+               (cardMarginRight * (cardCount / 2)) - 
+               carouselWidth - cardMarginRight);
+leftButton.addEventListener("click", function() {
+  if (offset !== 0) {
+    offset += carouselWidth + cardMarginRight;
+    carousel.style.transform = `translateX(${offset}px)`;
+    }
+})
+  
+rightButton.addEventListener("click", function() {
+  if (offset !== maxX) {
+    offset -= carouselWidth + cardMarginRight;
+    carousel.style.transform = `translateX(${offset}px)`;
+  }
+})
+imgContainer.addEventListener("click", function(event) {
     popup.style.display = 'block';
+    popupContent.src = event.target.src
 });
-LockPopUp.addEventListener("click", function() {
+closeButtonPopup.addEventListener("click", function() {
     popup.style.display = 'none';
 });
